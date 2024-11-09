8bea5a5b0eab0a8885ccb95ce1b27c5afdac2740
SohaibAShah <asyedsohaib@gmail.com>
2024-11-09 22:57:06 +0500

    Random Forest Added

---
+++ Supervised_Learning/5_Random_Forest/example.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from RandomForest import RandomForest
+
+
+bc = datasets.load_breast_cancer()
+X, y = bc.data, bc.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=421)
+
+clf = RandomForest(max_depth=5)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+def accuracy(y_test, y_pred):
+    return np.sum(y_test==y_pred)/len(y_test)
+
+accuracy = accuracy(y_test, y_pred)
+print(accuracy)
